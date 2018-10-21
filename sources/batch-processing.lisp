51a798b2fe28ce3fb4957de0a5fa5b1f9ba54288
David Stephen Grant <david@davidgrant.no>
2018-10-21 22:29:09 +0200

    Return filepaths from pad-eps-bounding-box.

--- sources/batch-processing.lisp
+++ sources/batch-processing.lisp
@@ -23,22 +23,21 @@
 (defmethod! omscore::pad-eps-bounding-box ((infile pathname) &optional (pad-size 5))
             :icon 700
   (let ((outfile (make-pathname :directory (pathname-directory infile) :name (concatenate 'string "pad_" (pathname-name infile)) :type (pathname-type infile))))
-        (with-open-file (eps-stream infile :direction :input)
-          (let ((data (loop for line = (read-line eps-stream nil nil)
-                            while line
-                            collect (let ((bb (clone (dsg::split-sequence '#\space line :remove-empty-subseqs t))))
-                                      (cond ((and (string= "%%BoundingBox:" (car bb))
-                                                  (eq (length bb) 5))
-                                             (setf (nth 1 bb) (- (parse-integer (nth 1 bb)) pad-size))
-                                             (setf (nth 2 bb) (- (parse-integer (nth 2 bb)) pad-size))
-                                             (setf (nth 3 bb) (+ (parse-integer (nth 3 bb)) pad-size))
-                                             (setf (nth 4 bb) (+ (parse-integer (nth 4 bb)) pad-size))
-                                             bb)
-                                            (t bb))))))
-            (om:save-data data outfile)))))
+    (with-open-file (eps-stream infile :direction :input)
+      (let ((data (loop for line = (read-line eps-stream nil nil)
+                        while line
+                        collect (let ((bb (clone (dsg::split-sequence '#\space line :remove-empty-subseqs t))))
+                                  (cond ((and (string= "%%BoundingBox:" (car bb))
+                                              (eq (length bb) 5))
+                                         (setf (nth 1 bb) (- (parse-integer (nth 1 bb)) pad-size))
+                                         (setf (nth 2 bb) (- (parse-integer (nth 2 bb)) pad-size))
+                                         (setf (nth 3 bb) (+ (parse-integer (nth 3 bb)) pad-size))
+                                         (setf (nth 4 bb) (+ (parse-integer (nth 4 bb)) pad-size))
+                                         bb)
+                                        (t bb))))))
+        (om:save-data data outfile)))))
 
 (defmethod! omscore::pad-eps-bounding-box ((infile-lst t) &optional (pad-size 5))
             :icon 700
   (loop for file in infile-lst
-        do (omscore::pad-eps-bounding-box file pad-size))
-  t)
+        collect (omscore::pad-eps-bounding-box file pad-size)))
